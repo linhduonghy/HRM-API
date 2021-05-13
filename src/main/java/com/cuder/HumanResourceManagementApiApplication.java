a1dd3f4f4889f31c12801b02abd5eaf29e58d177
linhduonghy <linhduonghy@gmail.com>
2021-05-13 08:43:06 +0700

    add project

---
+++ src/main/java/com/cuder/HumanResourceManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.cuder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourceManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanResourceManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cuder/HumanResourceManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cuder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourceManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
