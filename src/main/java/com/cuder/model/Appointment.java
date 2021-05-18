e2157401260574f81834c988eefe0c3301be62cd
unknown <linhhahoang1999@gmail.com>
2021-05-19 00:08:28 +0700

    a

--- src/main/java/com/cuder/model/Appointment.java
+++ src/main/java/com/cuder/model/Appointment.java
@@ -3,6 +3,9 @@ package com.cuder.model;
 import java.io.Serializable;
 import javax.persistence.*;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -35,6 +38,7 @@ public class Appointment implements Serializable {
 	private Manager manager;
 
 	//bi-directional many-to-one association to Staff
+	@JsonIgnoreProperties("appointments")
 	@ManyToOne
 	@JoinColumn(name = "staff_id")
 	private Staff staff;
